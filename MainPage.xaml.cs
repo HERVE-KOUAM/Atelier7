6c78809e5cd24e70e3d4fc95fa9319c3b2039149
KOUAM KAMDEM HERVE <hkouamherve145@gmail.com>
2024-11-23 03:23:06 -0500

    fin de la mise en place de la logique

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,14 +2,31 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+       
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-       
+        private void Fclic(object sender, EventArgs e)
+        {
+            if (int.TryParse(FaraE.Text, out int faraneit))
+            {
+                int celcus = (faraneit - 32) * 5 / 9;
+                CT.Text = celcus.ToString() + " 'C";
+
+            }
+        }
+
+        private void Celclic(object sender, EventArgs e)
+        {
+            if (int.TryParse(CelsusE.Text, out int celcus))
+            {
+                int faraneit = (celcus * 9 / 5) + 32;
+                FT.Text = faraneit.ToString()+" 'F";
+            }
+        }
     }
 
 }
